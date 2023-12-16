0bac0ad91d17ef5dd6893f2d6ca64ee5867a9829
TheRealJarbean <47651803+TheRealJarbean@users.noreply.github.com>
2023-12-16 12:18:46 -0500

    feat: Add searchable tags to recipes
    Implements feature from #4 
    
    * Implement tags in recipe dataclass
    
    * Implement tags in recipebook
    
    * Finish implementetion of searchable tags in uiux

--- src/recipebook.py
+++ src/recipebook.py
@@ -11,6 +11,7 @@ class RecipeBook:
 	def import_recipe(self, filepath: str):
 		recipe_data = {
 			"name": "",
+			"tags": [],
 			"source": "",
 			"description": "",
 			"ingredients": [], # Each element should be in the form: [name, quantity]
@@ -20,7 +21,8 @@ class RecipeBook:
 			lines = file.readlines()
 
 			recipe_data["name"] = lines[0].strip()[2:]
-			recipe_source = lines[1].strip()
+			recipe_data["tags"] = lines[1].split(', ')
+			recipe_source = lines[2].strip()
 			if recipe_source != "":
 				# Trim off the markdown link label and parenthesis
 				recipe_data["source"] = recipe_source.split("(")[1][:-1]
@@ -47,7 +49,7 @@ class RecipeBook:
 					current_line = current_line[3:] # Trim instruction number
 					recipe_data["instructions"].append(current_line)
 		
-		new_recipe = Recipe(recipe_data["name"], None, recipe_data["description"], recipe_data["ingredients"], recipe_data["instructions"])
+		new_recipe = Recipe(recipe_data["name"], recipe_data["tags"], None, recipe_data["description"], recipe_data["ingredients"], recipe_data["instructions"])
 		self.__recipes[recipe_data["name"].lower()] = new_recipe
 
 	# TODO: Add filtering, i.e. make parameters ingredients and strict functional
@@ -64,3 +66,13 @@ class RecipeBook:
 		# TODO: replace with try/catch for error handling
 		if recipe_name in self.get_recipe_names():
 			return self.__recipes[recipe_name].ingredients
+		
+	def search_recipes(self, search_terms: list[str]) -> list[Recipe]:
+		# Separate tags so they can be treated differently
+		tags = [term for term in search_terms if term.startswith('#')]
+		search_terms = [term for term in search_terms if term not in tags]
+		results = []
+		for key, recipe in self.__recipes.items():
+			if all(tag in recipe.tags for tag in tags) and all(term.lower() in recipe.name.lower() for term in search_terms):
+				results.append(key)
+		return results
