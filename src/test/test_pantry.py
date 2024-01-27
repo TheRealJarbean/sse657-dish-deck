5fb4851084b954ee374de1b096a066e99b42469d
Jacob K <jakekovarovic@gmail.com>
2024-01-27 01:24:33 -0600

    fix: TypeError for filepath

--- src/test/test_pantry.py
+++ src/test/test_pantry.py
@@ -5,9 +5,7 @@ from pantry import *
 
 @pytest.fixture
 def bad_filepath():
-    return{
-        'filepath' : '../data/recip3/diff.md'
-    }
+    return "../data/recip3/diff.md"
 
 @pytest.fixture
 def used_name():
@@ -27,41 +25,29 @@ def ex_ingredient():
 
 #Load Ingredients
 def test_load_ingredients(bad_filepath):
+    pantry = Pantry()
     try:
-        load_ingredients(bad_filepath)
+        pantry.load_ingredients(bad_filepath)
     except FileNotFoundError as error:
         pytest.fail(f"Execption raised: {error}")
 
 #Save ingredients
 def test_save_ingredients(bad_filepath):
+    pantry = Pantry()
     try:
-        save_ingredients(bad_filepath)
+        pantry.save_ingredients(bad_filepath)
     except FileNotFoundError as error:
         pytest.fail(f"Execption raised: {error}")
 
 #Add Ingredients
-@pytest.mark.dependency(depends=['test_load_ingredients'])
-def test_add_ingredient(tmp_path, ex_pantry):
-    directory = tmp_path / f'pantry/{ex_pantry["name"]}.json'
-    directory.parent.mkdir()
-    directory.touch()
-
-    pantry = Pantry()
-    pantry.load_ingredients(str(directory))
-
-    try:
-        pantry.add_ingredient(used_name)
-    except KeyError as error:
-        pytest.fail(f"Execption raised: {error}")
-
 def test_add_ingredient(ex_ingredient):
     pantry = Pantry()
     pantry.add_ingredient(ex_ingredient['name'],ex_ingredient['qty'],ex_ingredient['unit'])
 
-    storedIng = pantry.__ingredients[ex_ingredient['name']]
-    assert storedIng['name'] == ex_ingredient['name']
-    assert storedIng['qty'] == ex_ingredient['qty']
-    assert storedIng['unit'] == ex_ingredient['unit']
+    storedIng = pantry._ingredients[ex_ingredient['name']]
+    assert storedIng.name == ex_ingredient['name']
+    assert storedIng.quantity == ex_ingredient['qty']
+    assert storedIng.unit == ex_ingredient['unit']
 
     try:
         pantry.add_ingredient(ex_ingredient['name'])
