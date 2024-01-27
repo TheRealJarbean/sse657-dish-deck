231f1a5c7a9bd1bfc6b125c67d374d552b894e8d
Jacob K <jakekovarovic@gmail.com>
2024-01-27 00:24:06 -0600

    test-pantry: updated tests

--- src/test/test_pantry.py
+++ src/test/test_pantry.py
@@ -3,7 +3,6 @@ from pantry import *
 
 '''Testing Error Handling'''
 
-pantry = Pantry()
 @pytest.fixture
 def bad_filepath():
     return{
@@ -11,29 +10,60 @@ def bad_filepath():
     }
 
 @pytest.fixture
-def bad_name():
+def used_name():
     return{
-        'name' : 'spinach'
+        'name' : 'flour'
     }
+
+@pytest.fixture
+def ex_ingredient():
+    return{
+        
+        'name': 'flour',
+        'qty': '300',
+        'unit': 'grams'
+    }
+
+
 #Load Ingredients
-def test_load_ingredients():
+def test_load_ingredients(bad_filepath):
     try:
         load_ingredients(bad_filepath)
     except FileNotFoundError as error:
         pytest.fail(f"Execption raised: {error}")
 
 #Save ingredients
-def test_save_ingredients():
+def test_save_ingredients(bad_filepath):
     try:
         save_ingredients(bad_filepath)
     except FileNotFoundError as error:
         pytest.fail(f"Execption raised: {error}")
 
 #Add Ingredients
-def test_add_ingredient():
-    pantry.load_ingredients("test_file")
+@pytest.mark.dependency(depends=['test_load_ingredients'])
+def test_add_ingredient(tmp_path, ex_pantry):
+    directory = tmp_path / f'pantry/{ex_pantry["name"]}.json'
+    directory.parent.mkdir()
+    directory.touch()
+
+    pantry = Pantry()
+    pantry.load_ingredients(str(directory))
+
     try:
-        pantry.add_ingredient(bad_name)
+        pantry.add_ingredient(used_name)
     except KeyError as error:
         pytest.fail(f"Execption raised: {error}")
-        
+
+def test_add_ingredient(ex_ingredient):
+    pantry = Pantry()
+    pantry.add_ingredient(ex_ingredient['name'],ex_ingredient['qty'],ex_ingredient['unit'])
+
+    storedIng = pantry.__ingredients[ex_ingredient['name']]
+    assert storedIng['name'] == ex_ingredient['name']
+    assert storedIng['qty'] == ex_ingredient['qty']
+    assert storedIng['unit'] == ex_ingredient['unit']
+
+    try:
+        pantry.add_ingredient(ex_ingredient['name'])
+    except KeyError as error:
+        pytest.fail(f'Execption was raised: {error}')
